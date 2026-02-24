9af51c919960ddb4451b5b062ff9d6bf261e8b08
Fariasartuur <arturfarias49@gmail.com>
2026-02-24 15:32:20 -0300

    corrigindo alinhamento do card full

--- src/components/LangItem.js
+++ src/components/LangItem.js
@@ -1,8 +1,8 @@
 export const LangItem = (lang, i, yOffset = 0, focus) => {
     const isFocus = focus === lang.nome.toLowerCase();
     return `
-            <g transform="translate(0, ${i * 25 + yOffset})" ${isFocus ? `filter="drop-shadow(0 0 3px ${lang.cor})"` : ''}>
+            <g transform="translate(0, ${i * 30 + yOffset})" ${isFocus ? `filter="drop-shadow(0 0 3px ${lang.cor})"` : ''}>
                 <circle cx="5" cy="5" r="5" fill="${lang.cor}" />
-                <text x="20" y="9" class="lang-text">${lang.nome} - ${lang.porcentagem}% ${isFocus ? '🎯' : ''}</text>
+                <text x="20" y="10" class="lang-text">${lang.nome} - ${lang.porcentagem}% ${isFocus ? '🎯' : ''}</text>
             </g>`;
 }
